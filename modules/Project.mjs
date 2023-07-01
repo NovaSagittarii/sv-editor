4bfb4b0acd373fee5a6fb90e989d9175532fa9b1
NovaSagittarii <novasagittarii@gmail.com>
2023-06-30 20:19:19 -0600

    fix poor culling performance on large screens due to hardcoded offsets

--- modules/Project.mjs
+++ modules/Project.mjs
@@ -284,8 +284,8 @@ height:100vh;`;
   }
   refreshCulling(){ // return;
     const viewport = this.app.screen;
-    const minY = viewport.y - 1000;
-    const maxY = viewport.height + 500;
+    const minY = viewport.y - viewport.height*2;
+    const maxY = viewport.height;
     this.renderedMinT = this.t - (maxY - (this.app.view.height-100))/this.z;
     this.renderedMaxT = this.t - (minY + (this.app.view.height-100))/this.z;
     this.notes.forEach(n => {
